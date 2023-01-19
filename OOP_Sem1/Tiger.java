cfbfe6c05040a134dd77de23f0d2c7a2d383d0e6
ArtemMironov11 <artemmironov101191@g,ail.com>
2023-01-19 14:38:57 +0300

    OOP_HW1

---
+++ OOP_Sem1/Tiger.java
@@ -0,0 +1,14 @@
+package OOP_Sem1;
+
+public class Tiger extends Animal {
+    public void attack(Human human){
+        if (human.getStrength() > 5) {
+            human.setStrength(human.getStrength()-5);
+            System.out.println("Тигр атакует человека по имени " + human.getName());
+            System.out.println("У человека по имени " + human.getName() + " осталось " +
+                    human.getStrength() + " единиц энергии");
+        }
+        else System.out.println("Человек по имени " + human.getName() + " погибает или уже погиб");
+    }
+
+}
