e8d475108ab97b05798927def01cb17248589d28
ArtemMironov11 <artemmironov101191@g,ail.com>
2023-01-19 14:34:38 +0300

    OOP_HW1

---
+++ OOP_Sem1/TV.java
@@ -0,0 +1,11 @@
+package OOP_Sem1;
+
+public class TV {
+    public boolean isOn;
+    public void setOn(boolean On){
+        isOn = On;
+    }
+    public boolean isOn(){
+        return isOn;
+    }
+}
