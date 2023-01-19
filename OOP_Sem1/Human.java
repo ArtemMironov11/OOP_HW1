15d46cc9cbb6f07cc8c6294472b9f997bd94f95b
ArtemMironov11 <artemmironov101191@g,ail.com>
2023-01-19 10:00:57 +0300

    OOP_HW1

---
+++ OOP_Sem1/Human.java
@@ -0,0 +1,27 @@
+package OOP_Sem1;
+
+public class Human {
+
+    private String name;
+    private String sex;
+    private boolean liveStatus;
+
+
+    public Human(String name, String sex, boolean liveStatus) {
+        this.name = name;
+        this.sex = sex;
+        this.liveStatus = liveStatus;
+    }
+
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", liveStatus=" + liveStatus +
+                '}';
+    }
+}
