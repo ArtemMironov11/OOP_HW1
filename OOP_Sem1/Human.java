72754f1a8a0abb1e63a268789b1ecec24d8ce3cf
ArtemMironov11 <artemmironov101191@g,ail.com>
2023-02-05 13:10:14 +0300

    OOP_HW1

--- OOP_Sem1/Human.java
+++ OOP_Sem1/Human.java
@@ -1,12 +1,8 @@
 package OOP_Sem1;
 
 public class Human {
-
-
     private int strength;
-
     private String name;
-
     public void turnOnOffTV(TV tv) {
         if (this.strength > 5 && !tv.isOn()) {
             System.out.println("На включение ТВ потрачено 5 единиц энергии");
@@ -22,30 +18,24 @@ public class Human {
             System.out.println("Телевизор выключен");
         }else System.out.println("Ничего не выйдет, сил не осталось - пора спать!");
     }
-
     public void gotoSleep() {
         System.out.println(this.name + " лег спать" );
         System.out.println("Силы восстановлены");
         strength = 100;
     }
-
     public Human(String name) {
         this.name = name;
         this.strength = 100;
     }
-
     public int getStrength() {
         return strength;
     }
-
     public void setStrength(int strength) {
         this.strength = strength;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
